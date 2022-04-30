4927877b19a24b3f9d44ee6ff234c7c61ea36c7f
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-04-30 15:02:47 +0200

    socket.io set up

--- server/main.js
+++ server/main.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-app.get("/", (req, res) => {
-  res.send("bored yet?");
-  console.log("express is alive");
+// app.get("/", (req, res) => {
+//   res.send("bored yet?");
+//   console.log("express is alive");
+// });
+
+app.use(express.static("app"));
+
+io.on("connection", (socket) => {
+  console.log("socket.io is alive");
+  socket.emit("messages", ["hi", "yes", "no"]);
 });
 
 server.listen("80");
