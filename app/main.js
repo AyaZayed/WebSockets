6fb6486e1739f50e292e3e51873ca3517d9fe8ed
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-04-30 20:39:06 +0200

    input an likes done

--- app/main.js
+++ app/main.js
@@ -1,4 +1,58 @@
+var userId = localStorage.getItem("userId") || randomID();
+localStorage.setItem("userId", userId);
+console.info(userId);
+var messageCashe;
+
+function randomID() {
+  return Math.floor(Math.random() * 1e11);
+}
+
 var socket = io.connect("http://localhost", { forceNew: true });
 socket.on("messages", (data) => {
   console.info(data);
+  messageCashe = data;
+  render();
 });
+
+function render() {
+  var data = messageCashe;
+  var html = data
+    .map(function (d, index) {
+      return `
+      <form class='message' onsubmit="return likeMessage(messageCashe[${index}])">
+        <div class='name'>${d.userName}</div>
+        <a href=${d.content.link} class='message' target='blank'>${d.content.text}</a>
+        <input type='submit' class='likes-count' value="${d.likedBy.length} likes">
+      </form>
+      `;
+    })
+    .join(" ");
+  document.getElementById("messages").innerHTML = html;
+}
+
+function likeMessage(message) {
+  var index = message.likedBy.indexOf(userId);
+  if (index < 0) {
+    message.likedBy.push(userId);
+  } else {
+    message.likedBy.splice(index, 1);
+  }
+  socket.emit("update-message", message);
+  render();
+  return false;
+}
+
+function addMessage(e) {
+  var payload = {
+    userName: document.getElementById("username").value,
+    content: {
+      text: document.getElementById("message").value,
+      link: document.getElementById("linkAddress").value,
+    },
+    likedBy: [],
+    ts: Date.now(),
+  };
+  socket.emit("new-message", payload);
+  render();
+  return false;
+}
--- server/main.js
+++ server/main.js
@@ -12,7 +12,47 @@ app.use(express.static("app"));
 
 io.on("connection", (socket) => {
   console.log("socket.io is alive");
-  socket.emit("messages", ["hi", "yes", "no"]);
+  socket.emit("messages", messages);
+  socket.on("new-message", (data) => {
+    messages.push(data);
+    io.sockets.emit("messages", messages);
+  });
 });
+
+var messages = [
+  {
+    userId: 1,
+    messageId: 10,
+    userName: "Asha Greyjoy",
+    content: {
+      text: "The stone tree of the Stonetrees.",
+      link: "http://awoiaf.westeros.org/index.php/House_Stonetree",
+    },
+    likedBy: [1],
+    ts: Date.now() - 10000,
+  },
+  {
+    userId: 2,
+    messageId: 11,
+    userName: "Arya Stark",
+    content: {
+      text: "We'll come see this inn.",
+      link: "http://gameofthrones.wikia.com/wiki/Inn_at_the_Crossroads",
+    },
+    likedBy: [2, 3],
+    ts: Date.now() - 100000,
+  },
+  {
+    userId: 3,
+    messageId: 14,
+    userName: "Cersei Lannister",
+    content: {
+      text: "Her scheming forced this on me.",
+      link: "http://gameofthrones.wikia.com/wiki/Margaery_Tyrell",
+    },
+    likedBy: [],
+    ts: Date.now() - 1000000,
+  },
+];
 
 server.listen("80");
